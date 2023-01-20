c3b1681966b494599bd8e1426c93e06702f063f5
Andrés Gómez <gomez_andresf@hotmail.com>
2023-01-19 18:06:00 -0600

    Clase 209

---
+++ poo_interfaces/src/org/agomez/poointerfaces/imprenta/modelo/Curriculo.java
@@ -0,0 +1,44 @@
+package org.agomez.poointerfaces.imprenta.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curriculo extends Hoja{
+
+    private String persona;
+    private String carrera;
+    private List<String> experiencias;
+
+    public Curriculo(String persona, String carrera, String contenido) {
+        super(contenido);
+        this.persona = persona;
+        this.carrera = carrera;
+        this.experiencias = new ArrayList<>();
+    }
+
+    public Curriculo addExperiencias(String experiencia) {
+        this.experiencias.add(experiencia);
+        return this;
+    }
+
+    @Override
+    public String imprimir() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(persona)
+                .append("\n")
+                .append("Resumen: ").append(this.contenido)
+                .append("\n").append("Profesion: ")
+                .append(this.carrera).append("\n")
+                .append("Experiencia: \n");
+        /*
+        for(String exp: this.experiencias) {
+            sb.append("- ").append(exp).append("\n");
+        }
+        */
+
+        experiencias.forEach(exp -> {
+            sb.append("- ").append(exp).append("\n");
+        });
+        return sb.toString();
+    }
+}
